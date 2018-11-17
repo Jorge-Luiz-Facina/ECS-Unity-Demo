8bed90eab03caaf25b9426391fe57f37dbcc2e2d
MainDuelo <mainduelo@gmail.com>
2018-11-17 21:09:51 -0200

    first commit

---
+++ Assets/Scripts/ComponentSystem/InputSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class InputSystem : ComponentSystem {
+
+    private struct Data
+    {
+        public readonly int Length;
+        public ComponentArray<InputComponent> InputComponents;
+    }
+
+    [Inject] private Data  _data;
+
+    protected override void OnUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        for(int i = 0; i < _data.Length; i++)
+        {
+            _data.InputComponents[i].Horizontal = horizontal;
+            _data.InputComponents[i].Vertical = vertical;
+        }
+    }
+}
